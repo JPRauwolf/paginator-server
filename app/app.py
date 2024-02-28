e8a86f4c61fa11b8af74d34e22edcebc750acba2
JP100099 <JP100099@gmail.com>
2024-02-28 16:24:49 +0100

    so no cookie?

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 from typing import Annotated
 import secrets
 
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials, APIKeyQuery, APIKeyHeader
+from fastapi import FastAPI, Depends, HTTPException, status, Security
+from fastapi.security import HTTPBasic, HTTPBasicCredentials, APIKeyQuery, APIKeyHeader, APIKeyCookie
 
 app = FastAPI()
 
@@ -10,9 +10,35 @@ security = HTTPBasic()
 
 api_keys = ["1337"]
 
-#todo read from env variable
+# todo read from env variable
 ADMIN_USER = b"jp"
 ADMIN_PASSWORD = b"password"
+
+# cookie_key = APIKeyCookie(name="key", auto_error=False)
+header_key = APIKeyHeader(name="x-api-key", auto_error=False)
+query_key = APIKeyQuery(name="api-key", auto_error=False)
+
+
+def get_api_key(
+    # cookie_key: str = Security(cookie_key),
+    header_key: str = Security(header_key),
+    query_key: str = Security(query_key)
+) -> str:
+
+    # if cookie_key in api_keys:
+    #    return cookie_key
+
+    if header_key in api_keys:
+        return header_key
+
+    if query_key in api_keys:
+        return query_key
+    
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="api key missing or incorrect"
+    )
+
 
 def passwd_auth(
     credentials: Annotated[HTTPBasicCredentials, Depends(security)]
@@ -25,13 +51,18 @@ def passwd_auth(
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="wrong username or password",
-            headers={"WWW-Authenticate" : "Basic"}
+            headers={"WWW-Authenticate": "Basic"}
         )
     return credentials.username
-    
+
 
 @app.get("/api/generatekey")
 def read_current_user(username: Annotated[HTTPBasicCredentials, Depends(passwd_auth)]):
     return {"username": username}
 
 
+@app.get("/")
+def root(
+    api_key: str = Depends(get_api_key)
+):
+    return {"api-key": api_key}
